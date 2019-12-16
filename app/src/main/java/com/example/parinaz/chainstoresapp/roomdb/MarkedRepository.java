a3f7445aab94cf30087f64076cc7a2d207c298ea
parinazasghari <parinazasghari75@gmail.com>
2019-12-16 16:59:07 +0330

    repository creating

--- app/src/main/java/com/example/parinaz/chainstoresapp/roomdb/MarkedRepository.java
+++ app/src/main/java/com/example/parinaz/chainstoresapp/roomdb/MarkedRepository.java
@@ -1,4 +1,29 @@
 package com.example.parinaz.chainstoresapp.roomdb;
 
+import android.app.Application;
+import android.arch.lifecycle.LiveData;
+import android.content.Context;
+
+import java.util.List;
+
 public class MarkedRepository {
+    private markedDAO mDAO;
+    private LiveData<List<markedEntity>> mAllmarked;
+    Context context;
+
+    public MarkedRepository(Application application) {
+        markedDatabase db =  markedDatabase.getInstance(application);
+        mDAO=db.markedDAO();
+        mAllmarked=mDAO.getAll();
+
+    }
+
+    LiveData<List<markedEntity>> getmAllmarked(){
+        return mAllmarked;
+    }
+
+    void  insert(markedEntity markedEntity){
+        markedDatabase.getInstance(context).markedDAO().insert(markedEntity);
+
+    }
 }
