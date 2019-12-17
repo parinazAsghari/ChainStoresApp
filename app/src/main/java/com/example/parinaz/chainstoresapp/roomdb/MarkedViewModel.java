3dc05b372a42292bf6fcc87d493902ecf2510866
parinazasghari <parinazasghari75@gmail.com>
2019-12-17 13:37:30 +0330

    ismarked and delete method inserted

--- app/src/main/java/com/example/parinaz/chainstoresapp/roomdb/MarkedViewModel.java
+++ app/src/main/java/com/example/parinaz/chainstoresapp/roomdb/MarkedViewModel.java
@@ -3,6 +3,7 @@ package com.example.parinaz.chainstoresapp.roomdb;
 import android.app.Application;
 import android.arch.lifecycle.AndroidViewModel;
 import android.arch.lifecycle.LiveData;
+import android.content.Context;
 import android.support.annotation.NonNull;
 
 import java.util.List;
@@ -12,16 +13,27 @@ public class MarkedViewModel extends AndroidViewModel {
 private LiveData<List<markedEntity>> mAllmarked;
 
 
-    public MarkedViewModel(@NonNull Application application) {
+
+
+  public MarkedViewModel(Application application) {
         super(application);
         markedRepository = new MarkedRepository(application);
         mAllmarked=markedRepository.getmAllmarked();
     }
 
-    LiveData<List<markedEntity>> getmAllmarked(){
+   public LiveData<List<markedEntity>> getmAllmarked(){
         return mAllmarked;
     }
     public void insert(markedEntity markedEntity){
         markedRepository.insert(markedEntity);
+    }
+
+    public void delete(int code , int branchid){
+      markedRepository.delete(code, branchid);
+    }
+
+
+    public boolean ismarked(int code, int branchid){
+     return markedRepository.ismarked(code, branchid);
     }
 }
