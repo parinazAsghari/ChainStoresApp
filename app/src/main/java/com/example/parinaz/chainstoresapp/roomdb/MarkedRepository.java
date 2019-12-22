8aa5ec030b0f07ea66bcbc7a2a4a47740d3dfaa5
parinazasghari <parinazasghari75@gmail.com>
2019-12-22 13:49:32 +0330

    iplement singleton on repository class

--- app/src/main/java/com/example/parinaz/chainstoresapp/roomdb/MarkedRepository.java
+++ app/src/main/java/com/example/parinaz/chainstoresapp/roomdb/MarkedRepository.java
@@ -2,36 +2,81 @@ package com.example.parinaz.chainstoresapp.roomdb;
 
 import android.app.Application;
 import android.arch.lifecycle.LiveData;
+import android.arch.lifecycle.MutableLiveData;
 import android.content.Context;
+import android.os.AsyncTask;
+import android.os.Build;
+import android.support.annotation.AnyThread;
+import android.support.annotation.MainThread;
+import android.support.annotation.RequiresApi;
+import android.support.annotation.WorkerThread;
 
 import java.util.List;
 
+//singleton
 public class MarkedRepository {
+
+    private static MarkedRepository instance;
     private markedDAO mDAO;
     private LiveData<List<markedEntity>> mAllmarked;
     Context context;
 
-    public MarkedRepository(Application application) {
-        markedDatabase db =  markedDatabase.getInstance(application);
-        mDAO=db.markedDAO();
-        mAllmarked=mDAO.getAll();
+    private MarkedRepository() {
 
+        if (instance != null) {
+            throw new RuntimeException("getinstance()");
+        }
     }
 
-    LiveData<List<markedEntity>> getmAllmarked(){
+    public static MarkedRepository getinstance() {
+        if (instance == null) {
+            instance = new MarkedRepository();
+
+        }
+        return instance;
+    }
+
+
+    public void init(Context context) {
+        markedDatabase db = markedDatabase.getInstance(context);
+        mDAO = db.markedDAO();
+        mAllmarked = mDAO.getAll();
+    }
+
+
+    LiveData<List<markedEntity>> getmAllmarked() {
         return mAllmarked;
     }
 
-    void  insert(markedEntity markedEntity){
+
+    //@WorkerThread
+    //@RequiresApi(api = Build.VERSION_CODES.CUPCAKE)
+    void insert(markedEntity markedEntity) {
+
+
+        //  new AsyncTask() {
+
+        //       @Override
+        //     protected Object doInBackground(Object[] objects) {
         markedDatabase.getInstance(context).markedDAO().insert(markedEntity);
-
     }
+        //}
+        //}.execute();
+        // print time;
+        //}
 
-    void delete(int code , int branchid){
-        markedDatabase.getInstance(context).markedDAO().deleteAll(code, branchid);
-    }
+        //@RequiresApi(api = Build.VERSION_CODES.CUPCAKE)
+       // void insert (markedEntity markedEntity){
+         //   markedDatabase.getInstance(context).markedDAO().insert(markedEntity);
+            //  print time;
+            //}*/
 
-    boolean ismarked(int code , int branchid){
-       return markedDatabase.getInstance(context).markedDAO().isMarked(code, branchid);
-    }
-}
+            void delete ( int code, int branchid){
+                markedDatabase.getInstance(context).markedDAO().deleteAll(code, branchid);
+            }
+
+            boolean ismarked ( int code, int branchid){
+                return markedDatabase.getInstance(context).markedDAO().isMarked(code, branchid);
+            }
+        }
+
