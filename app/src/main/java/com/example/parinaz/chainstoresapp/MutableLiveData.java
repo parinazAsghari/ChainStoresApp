a9551d47930d4485a7dbcbc75c138cdb88d08688
parinazasghari <parinazasghari75@gmail.com>
2019-12-23 16:32:30 +0330

    create MutableLivaData.java class whith post method

--- app/src/main/java/com/example/parinaz/chainstoresapp/MutableLiveData.java
+++ app/src/main/java/com/example/parinaz/chainstoresapp/MutableLiveData.java
@@ -1,4 +1,11 @@
 package com.example.parinaz.chainstoresapp;
 
-public class MutableLiveData {
+import android.arch.lifecycle.LiveData;
+
+public class MutableLiveData<T> extends LiveData<T> {
+
+    @Override
+    public void postValue(T value) {
+        super.postValue(value);
+    }
 }
