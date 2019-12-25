bf7d9823d730153b94187028ec5402a41f8c9c17
parinazasghari <parinazasghari75@gmail.com>
2019-12-25 17:34:10 +0330

    asynctask of methods

--- app/src/main/java/com/example/parinaz/chainstoresapp/roomdb/MarkedRepository.java
+++ app/src/main/java/com/example/parinaz/chainstoresapp/roomdb/MarkedRepository.java
@@ -1,25 +1,28 @@
 package com.example.parinaz.chainstoresapp.roomdb;
 
-import android.app.Application;
+import android.app.Activity;
 import android.arch.lifecycle.LiveData;
-import android.arch.lifecycle.MutableLiveData;
 import android.content.Context;
+import android.database.Cursor;
 import android.os.AsyncTask;
-import android.os.Build;
-import android.support.annotation.AnyThread;
-import android.support.annotation.MainThread;
-import android.support.annotation.RequiresApi;
+import android.support.annotation.BinderThread;
 import android.support.annotation.WorkerThread;
 
+import com.example.parinaz.chainstoresapp.Runnable;
+
 import java.util.List;
+import java.util.concurrent.ExecutionException;
 
 //singleton
 public class MarkedRepository {
+
+
 
     private static MarkedRepository instance;
     private markedDAO mDAO;
     private LiveData<List<markedEntity>> mAllmarked;
     Context context;
+
 
     private MarkedRepository() {
 
@@ -38,9 +41,11 @@ public class MarkedRepository {
 
 
     public void init(Context context) {
-        markedDatabase db = markedDatabase.getInstance(context);
-        mDAO = db.markedDAO();
-        mAllmarked = mDAO.getAll();
+
+                markedDatabase db = markedDatabase.getInstance(context);
+                mDAO = db.markedDAO();
+                mAllmarked = mDAO.getAll();
+
     }
 
 
@@ -51,32 +56,90 @@ public class MarkedRepository {
 
     //@WorkerThread
     //@RequiresApi(api = Build.VERSION_CODES.CUPCAKE)
-    void insert(markedEntity markedEntity) {
 
+  /* void insert(markedEntity markedEntity) {
+        new AsyncTask() {
+            @Override
+            protected Object doInBackground(Object[] objects) {
+                markedDatabase.getInstance(context).markedDAO().insert(markedEntity);
+                return null;
+            }
+        }.execute();
 
-        //  new AsyncTask() {
+    }*/
 
-        //       @Override
-        //     protected Object doInBackground(Object[] objects) {
-        markedDatabase.getInstance(context).markedDAO().insert(markedEntity);
+    public void insert(final markedEntity markedEntity, java.lang.Runnable runnable) {
+        new AsyncTask<Void, Void, Void>() {
+            @Override
+            protected Void doInBackground(Void... voids) {
+                markedDatabase.getInstance(context).markedDAO().insert(markedEntity);
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(Void aVoid) {
+                super.onPostExecute(aVoid);
+                runnable.run();
+            }
+
+        }.execute();
+
+        
     }
-        //}
-        //}.execute();
-        // print time;
-        //}
 
-        //@RequiresApi(api = Build.VERSION_CODES.CUPCAKE)
-       // void insert (markedEntity markedEntity){
-         //   markedDatabase.getInstance(context).markedDAO().insert(markedEntity);
-            //  print time;
-            //}*/
 
-            void delete ( int code, int branchid){
-                markedDatabase.getInstance(context).markedDAO().deleteAll(code, branchid);
+    public void delete(final int code , final int branchid , java.lang.Runnable runnable) {
+
+            new AsyncTask<Void, Void, Void>() {
+                @Override
+                protected Void doInBackground(Void... voids) {
+                   // noteDatabase.daoAccess().deleteTask(task.getValue());
+                    markedDatabase.getInstance(context).markedDAO().deleteAll(code, branchid);
+                    return null;
+            }
+                @Override
+                protected void onPostExecute(Void aVoid) {
+                    super.onPostExecute(aVoid);
+                    runnable.run();
+                }
+        }.execute();
+    }
+
+
+   /* void delete ( int code, int branchid){
+                new AsyncTask() {
+                    @Override
+                    protected Object doInBackground(Object[] objects) {
+                        markedDatabase.getInstance(context).markedDAO().deleteAll(code, branchid);
+                        return  null;
+                    }
+                }.execute();
+
+
+                    }*/
+
+
+
+           void ismarked (int code, int branchid , Runnable runnable) {
+             //  ,Runnable runnable
+                new AsyncTask<Void,Void,Boolean>() {
+
+                    @Override
+                    protected Boolean doInBackground(Void... voids) {
+
+                      return markedDatabase.getInstance(context).markedDAO().isMarked(code, branchid);
+
+                    }
+
+                    @Override
+                    protected void onPostExecute(Boolean aBoolean) {
+                        super.onPostExecute(aBoolean);
+                        runnable.run(aBoolean);
+                    }
+                }.execute();
+
+            // return markedDatabase.getInstance(context).markedDAO().isMarked(code, branchid);
             }
 
-            boolean ismarked ( int code, int branchid){
-                return markedDatabase.getInstance(context).markedDAO().isMarked(code, branchid);
-            }
         }
 
