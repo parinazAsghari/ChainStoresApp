1a3f75a248d90eb9286f2e895e3060a2c04c6633
parinazasghari <parinazasghari75@gmail.com>
2020-01-04 11:55:31 +0330

    sabadkala interface

--- app/src/main/java/com/example/parinaz/chainstoresapp/object/SabadKalaInterface.java
+++ app/src/main/java/com/example/parinaz/chainstoresapp/object/SabadKalaInterface.java
@@ -1,4 +1,20 @@
 package com.example.parinaz.chainstoresapp.object;
 
+import java.util.List;
+
 public interface SabadKalaInterface {
+
+    int getRowPrice(List<Product> priceList );//قیمت خالص
+
+  int getProductsQuantity(List<Product> products);//تعداد محصولات سبد کالا
+
+    int getPriceWithDiscount(List<Product> products);//قیمت با محاسبه تخفیف
+
+   List<Product> getPoducts(List<Product> products);
+
+    void addProduct(Product product);
+
+    void removeProduct(Product product);
+
+    Product getProductByCode(int code);
 }
